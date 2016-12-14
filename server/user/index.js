d2c594215f2c8d02ad31846c0041e0267d59ca16
r45r54r45 <r45r54r45@yonsei.ac.kr>
2016-12-15 00:34:32 +0900

    fixed untracked files

--- server/user/index.js
+++ server/user/index.js
@@ -1,18 +1,18 @@
-import { Router } from 'express';
+import {Router} from 'express';
 import {query} from '../modules/mysql'
-let router= Router();
+let router = Router();
 router
-    .get('/login',(req, res)=>{
-    "use strict";
-    query("select * from test where id=?",[1]).then(data=>{
-        res.json(data);
-    });
-})
-    .post('/',(req, res)=>{
+    .get('/login', (req, res)=> {
+        "use strict";
+        query("select * from test where id=?", [1]).then(data=> {
+            res.json(data);
+        });
+    })
+    .post('/', (req, res)=> {
         "use strict";
 
     })
 
-export default ()=>{
+export default ()=> {
     return router;
 }
